1abb38445c1c8c113c0bc1512678987c93ebb525
Kevin Surmacewicz <surmackc@gmail.com>
2018-05-17 00:14:22 -0400

    Update Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import "./Nav.css";
 const Nav = props => (
   <div>
     <ul className="nav nav-pills nav-justified">
-      <li><a href="/">Start</a></li>
+      <li><a href="https://surmackc.github.io/Space-Shuffle/">Start</a></li>
       <li>Score: <span style={{color: "yellow"}}>{props.curScore}</span> | Top Score: <span style={{color: "pink"}}>{props.topScore}</span>
       </li>
     </ul>
